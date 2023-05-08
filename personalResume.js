44681374e89567603299918b8e1bbde015e1f413
DaryllCulas <culasdaryll99@gmail.com>
2023-05-08 13:29:47 +0800

    Created codes in personalResume.js

---
+++ personalResume.js
@@ -0,0 +1,10 @@
+var navLinks = document.querySelectorAll("nav nav-links li a");
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function() {
+    for (var j = 0; j < navLinks.length; j++) {
+      navLinks[j].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
