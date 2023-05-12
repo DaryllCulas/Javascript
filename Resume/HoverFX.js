ee31364e95f032df25212f31d5816b60a6d21a1f
DaryllCulas <culasdaryll99@gmail.com>
2023-05-12 18:46:56 +0800

    Replaced all files to Resume Dir && Rename a file into HoverFX.js

---
+++ Resume/HoverFX.js
@@ -0,0 +1,16 @@
+
+/* Hover effects that stays highlighted and active its lists when selected */
+var navLinks = document.querySelectorAll(".nav-links li a");
+
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function() {
+    for (var j = 0; j < navLinks.length; j++) {
+      navLinks[j].classList.remove("active");
+    }
+    this.classList.add("active");
+   
+  });
+
+  
+}
